0fe411bb27a4b39349b65472046ee8cd18364710
Pedro Josue <pedro.josue.ap@outlook.com>
2024-09-24 20:06:44 -0300

    criando as rotas na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Login from './routes/Login.jsx'
+import Musicas from './routes/Musicas.jsx'
 
+//criando a função brouser router
+
+const router =createBrowserRouter([
+  {
+    //chamando o elemento pai
+    path:'/',element:<App/>,
+    errorElement:<Error/>,
+    //elementos filhos
+    children:[
+      {path:'/', element:<Home/>},
+      {path:'/musicas', element:<Musicas/>},
+      {path:'/login', element:<Login/>},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
