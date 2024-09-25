6716495d2b9395bf85248eb04c57959aef2ed4b9
Pedro Josue <pedro.josue.ap@outlook.com>
2024-09-25 11:51:08 -0300

    adicionando cor ao fundo da home

--- src/css/HomeStyle.jsx
+++ src/css/HomeStyle.jsx
@@ -5,10 +5,12 @@ export const HomeStyle = styled.section`
 .home h1 {
     margin-top: 7%;
     font-size: 400%;
+    color: white;
 }
 .home h3 {
     font-size: 150%;
     margin-bottom: 7%;
+    color: white;
 }
 .home {
     position: relative;
@@ -21,6 +23,7 @@ export const HomeStyle = styled.section`
     padding: 20px;
     box-sizing: border-box;
     z-index: 1;
+    background-color: #1a1a4b;
     font-family: 'Roboto', sans-serif;
 }
 .btn-home {
